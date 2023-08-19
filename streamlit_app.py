f2e58445601cd11b76a6589fadc6f6c70cc44897
RajasekharReddy-Muthumala <142430196+RajasekharReddy-Muthumala@users.noreply.github.com>
2023-08-19 23:25:04 +0530

    Update streamlit_app.py
    check error

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,9 @@ from urllib.error import URLError
 streamlit.header("Fruityvice Fruit Advice!")
 try:
     fruit_choice=streamlit.text_input('What fruit would you like information about?','Kiwi')
-if not Fruit_choice:
+  if not Fruit_choice:
 streamlit.error("please select a fruit to get a information.")
-else:
+ else:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
