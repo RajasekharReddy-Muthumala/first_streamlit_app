36a5e0aea499973089c5c6009025f95dc0123c30
RajasekharReddy-Muthumala <142430196+RajasekharReddy-Muthumala@users.noreply.github.com>
2023-08-19 23:30:09 +0530

    Update streamlit_app.py
    identation error

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,13 +22,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 from urllib.error import URLError
-
 streamlit.header("Fruityvice Fruit Advice!")
-try  : 
-    fruit_choice=streamlit.text_input('What fruit would you like information about?','Kiwi')
-  if not Fruit_choice    :
+try: 
+fruit_choice=streamlit.text_input('What fruit would you like information about?','Kiwi')
+if not Fruit_choice:
 streamlit.error("please select a fruit to get a information.")
- else  :
+else:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
